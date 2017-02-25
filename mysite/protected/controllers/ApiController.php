bb95ed4a685fd00f96e82bea05d32d7568ee2029
classicalguss <barghouti_since88@hotmail.com>
2017-02-25 15:22:25 +0200

    Modifications on register and switching functions order

--- mysite/protected/controllers/ApiController.php
+++ mysite/protected/controllers/ApiController.php
@@ -24,57 +24,117 @@ class ApiController extends Controller
 		if (!Yii::app()->user->isGuest)
 			echo json_encode(Yii::app()->user->user_name);
 	}
-	public function actionAddstory()
+	
+	/**
+	 * For registering User through API
+	 */
+	public function actionRegister()
 	{
-		if (Yii::app()->user->isGuest == true)
+		if (Yii::app()->user->isGuest == false)
 		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'Not logged in, please log in first'));
+			echo json_encode(array('status' =>'error','error_code'=>6,'message'=>'Need to logout before registering a new account'));
 			Yii::app()->end();
 		}
 		
-		$type = Yii::app()->request->getPost('type',null);
-		$title = Yii::app()->request->getPost('title',null);
-		$hashtag = Yii::app()->request->getPost('hashtag',null);
-		$content = Yii::app()->request->getPost('content',null);
-		
-		if (empty($type))
+		$deviceToken = Yii::app()->request->getParam('device_token',null);
+		$deviceType 	= Yii::app()->request->getParam('device_type',null);
+	
+		if ($deviceToken == null)
 		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'type was not provided'));
+			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'device_token not provided.'));
 			Yii::app()->end();
 		}
-		else if (empty($title))
+		else if ($deviceType == null)
 		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'title was not provided'));
+			echo json_encode(array('status' =>'error','error_code'=>2,'message'=>'device_type not provided.'));
 			Yii::app()->end();
 		}
-		else if (empty($content))
+	
+		$criteria = new CDbCriteria();
+		$criteria->addCondition('device_token = :device_token');
+		$criteria->addCondition('device_type = :device_type');
+		$criteria->params = array(':device_token'=>$deviceToken,':device_type'=>$deviceType);
+	
+		$user = new User();
+		$user = User::model()->find($criteria);
+	
+		if ($user && $user->user_name != null)
 		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'content was not provided'));
+			echo json_encode(array('status' =>'user_exist','user_name'=>$user->user_name));
 			Yii::app()->end();
 		}
-		
-		$story = new UserStory();
-		$story->user_id = Yii::app()->user->id;
-		$story->title = $title;
-		$story->type = $type;
-		$story->hashtags = '#'.implode(' #',explode(',',$hashtag));
-		$story->content = $content;
+	
+		$user = new User();
+		$user->user_name = substr( md5(rand()), 0, 15);
+		$user->device_type = $deviceType;
+		$user->device_token = $deviceToken;
+	
 		try
 		{
-			$story->save();
-			var_dump($story->errors);
+			$user->save();
 		}
 		catch (CDbException $e)
 		{
-			echo json_encode(array('status' =>'error','error_code'=>4,'error'=>$e));
+			echo json_encode(array('status' =>'error','error_code'=>3,'error'=>$e));
 			Yii::app()->end();
 		}
-		echo json_encode(array('status' =>'success','story'=>$story->getAttributes()));
+	
+		if($user->hasErrors())
+		{
+			echo json_encode(array('status' =>'error','error_code'=>4,'error'=>$user->errors));
+			Yii::app()->end();
+		}
+		
+		$identity=new UserIdentity($user->user_name,$deviceToken);
+		
+		if($identity->authenticate())
+		{
+			Yii::app()->user->login($identity);
+			echo json_encode(array('status' =>'success','user_id'=>Yii::app()->user->id,'user_name'=>Yii::app()->user->user_name));
+		}
+		else
+			echo json_encode(array('status' =>'error','error_code'=>5,'message'=>'Authentication Error.'));
 	}
+	
+	/**
+	 * For logging in the User
+	 */
+	public function actionLogin()
+	{
+		if (Yii::app()->user->isGuest == false)
+		{
+			echo json_encode(array('status' =>'logged_in','message'=>'This user is already logged in'));
+			Yii::app()->end();
+		}
+		$deviceToken 	= Yii::app()->request->getPost('device_token',null);
+		$userName		= Yii::app()->request->getPost('user_name',null);
+	
+		if ($userName == null)
+		{
+			echo json_encode(array('status' =>'error','error_code'=>2,'message'=>'user_name not provided.'));
+			Yii::app()->end();
+		}
+		else if ($deviceToken == null)
+		{
+			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'device_token not provided.'));
+			Yii::app()->end();
+		}
+	
+		$identity=new UserIdentity($userName,$deviceToken);
+		if($identity->authenticate())
+		{
+			Yii::app()->user->login($identity);
+			echo json_encode(array('status' =>'success','id'=>Yii::app()->user->id,'user_name'=>Yii::app()->user->user_name));
+		}
+		else
+			echo json_encode(array('status' =>'error','error_code'=>3,'message'=>'Authentication Error.'));
+	}
+	
+
 	/**
 	 * For getting user interests
 	 */
-	public function actionUserstories()
+	public function actionUserinterests()
 	{
 		$userId 	= Yii::app()->request->getParam('user_id',null);
 		if ($userId == null)
@@ -82,15 +142,17 @@ class ApiController extends Controller
 			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'user_id not provided.'));
 			Yii::app()->end();
 		}
-		$userStories = UserStory::model()->findAllByAttributes(array('user_id'=>$userId));
+		$userInterests = UserInterest::model()->findAllByAttributes(array('user_id'=>$userId));
 	
-		$stories = array();
-		foreach ($userStories as $userStory)
+		$interests = array();
+		foreach ($userInterests as $userInterest)
 		{
-			$stories[] = $userStory->getAttributes();
+			$interests[] = $userInterest->interest;
 		}
-		echo json_encode(array('status' =>'success','stories'=>$stories));
+		echo json_encode(array('status' =>'success','interests'=>$interests));
+		Yii::app()->end();
 	}
+
 	/**
 	 * For adding User interests
 	 */
@@ -101,7 +163,7 @@ class ApiController extends Controller
 			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'Not logged in, please log in first'));
 			Yii::app()->end();
 		}
-		
+	
 		$interestList 	= Yii::app()->request->getPost('interests',array());
 		if (count($interestList) > 50)
 		{
@@ -113,11 +175,11 @@ class ApiController extends Controller
 			echo json_encode(array('status' =>'error','error_code'=>3,'message'=>'didnt supply an array of interests'));
 			Yii::app()->end();
 		}
-		
+	
 		UserInterest::model()->deleteAllByAttributes(array('user_id'=>Yii::app()->user->id)); //Deleting all the interests so that we can reset them
-		
+	
 		$userInterests = array();
-		
+	
 		foreach ($interestList as $interest)
 		{
 			$userInterest = new UserInterest();
@@ -134,31 +196,84 @@ class ApiController extends Controller
 			}
 			$userInterests[] = $interest;
 		}
-		
+	
 		echo json_encode(array('status' =>'success','interests'=>$userInterests));
 	}
+	
+	/**
+	 * For adding a user story
+	 */
+	public function actionAddstory()
+	{
+		if (Yii::app()->user->isGuest == true)
+		{
+			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'Not logged in, please log in first'));
+			Yii::app()->end();
+		}
+	
+		$type = Yii::app()->request->getPost('type',null);
+		$title = Yii::app()->request->getPost('title',null);
+		$hashtag = Yii::app()->request->getPost('hashtag',null);
+		$content = Yii::app()->request->getPost('content',null);
+	
+		if (empty($type))
+		{
+			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'type was not provided'));
+			Yii::app()->end();
+		}
+		else if (empty($title))
+		{
+			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'title was not provided'));
+			Yii::app()->end();
+		}
+		else if (empty($content))
+		{
+			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'content was not provided'));
+			Yii::app()->end();
+		}
+	
+		$story = new UserStory();
+		$story->user_id = Yii::app()->user->id;
+		$story->title = $title;
+		$story->type = $type;
+		$story->hashtags = '#'.implode(' #',explode(',',$hashtag));
+		$story->content = $content;
+		try
+		{
+			$story->save();
+		}
+		catch (CDbException $e)
+		{
+			echo json_encode(array('status' =>'error','error_code'=>4,'error'=>$e));
+			Yii::app()->end();
+		}
+		echo json_encode(array('status' =>'success','story'=>$story->getAttributes()));
+	}
+	
 	/**
 	 * For getting user interests
 	 */
-	public function actionUserinterests()
+	public function actionUserstories()
 	{
 		$userId 	= Yii::app()->request->getParam('user_id',null);
-		if ($userId == null)
-		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'user_id not provided.'));
-			Yii::app()->end();
-		}
-		$userInterests = UserInterest::model()->findAllByAttributes(array('user_id'=>$userId));
 		
-		$interests = array();
-		foreach ($userInterests as $userInterest)
+		$criteria = new CDbCriteria();
+		if($userId !== null)
 		{
-			$interests[] = $userInterest->interest;
+			$criteria->addInCondition('user_id',array($userId));
 		}
-		echo json_encode(array('status' =>'success','interests'=>$interests));
-		Yii::app()->end();
+		$criteria->order = 'posted_date DESC';
+		
+		$userStories = UserStory::model()->findAll($criteria);
+		
+		$stories = array();
+		foreach ($userStories as $userStory)
+		{
+			$stories[] = $userStory->getAttributes();
+		}
+		echo json_encode(array('status' =>'success','stories'=>$stories));
 	}
-	
+
 	/**
 	 * For Logging out the User
 	 */
@@ -166,96 +281,5 @@ class ApiController extends Controller
 	{
 		Yii::app()->user->logout();
 		echo json_encode(array('status'=>'success'));
-	}
-	
-	/**
-	 * For logging in the User
-	 */
-	public function actionLogin()
-	{
-		if (Yii::app()->user->isGuest == false)
-		{
-			echo json_encode(array('status' =>'logged_in','message'=>'This user is already logged in'));
-			Yii::app()->end();
-		}
-		$deviceToken 	= Yii::app()->request->getPost('device_token',null);
-		$userName		= Yii::app()->request->getPost('user_name',null);
-		
-		if ($userName == null)
-		{
-			echo json_encode(array('status' =>'error','error_code'=>2,'message'=>'user_name not provided.'));
-			Yii::app()->end();
-		}
-		else if ($deviceToken == null)
-		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'device_token not provided.'));
-			Yii::app()->end();
-		}
-		
-		$identity=new UserIdentity($userName,$deviceToken);
-		if($identity->authenticate())
-		{
-			Yii::app()->user->login($identity);
-			echo json_encode(array('status' =>'success','id'=>Yii::app()->user->id,'user_name'=>Yii::app()->user->user_name));
-		}
-		else
-			echo json_encode(array('status' =>'error','error_code'=>3,'message'=>'Authentication Error.'));
-	}
-	
-	/**
-	 * For registering User through API
-	 */
-	public function actionRegister() 
-	{
-		$deviceToken = Yii::app()->request->getParam('device_token',null);
-		$deviceType 	= Yii::app()->request->getParam('device_type',null);
-		
-		if ($deviceToken == null)
-		{
-			echo json_encode(array('status' =>'error','error_code'=>1,'message'=>'device_token not provided.'));
-			Yii::app()->end();
-		}
-		else if ($deviceType == null)
-		{
-			echo json_encode(array('status' =>'error','error_code'=>2,'message'=>'device_type not provided.'));
-			Yii::app()->end();
-		}
-		
-		$criteria = new CDbCriteria();
-		$criteria->addCondition('device_token = :device_token');
-		$criteria->addCondition('device_type = :device_type');
-		$criteria->params = array(':device_token'=>$deviceToken,':device_type'=>$deviceType);
-		
-		$user = new User();
-		$user = User::model()->find($criteria);
-		
-		if ($user && $user->user_name != null)
-		{
-			echo json_encode(array('status' =>'user_exist','user_name'=>$user->user_name));
-			Yii::app()->end();
-		}
-		
-		$user = new User();
-		$user->user_name = substr( md5(rand()), 0, 15);
-		$user->device_type = $deviceType;
-		$user->device_token = $deviceToken;
-		
-		try
-		{
-			$user->save();
-		}
-		catch (CDbException $e)
-		{
-			echo json_encode(array('status' =>'error','error_code'=>3,'error'=>$e));
-			Yii::app()->end();
-		}
-		
-		if($user->hasErrors())
-		{
-			echo json_encode(array('status' =>'error','error_code'=>4,'error'=>$user->errors));
-			Yii::app()->end();
-		}
-		
-		echo json_encode(array('status' => 'success','user_name'=>$user->user_name));
 	}
 }
